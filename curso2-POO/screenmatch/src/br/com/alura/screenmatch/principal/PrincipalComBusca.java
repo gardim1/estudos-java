62c3cc3cc09843780222780c374efc45ee6b3493
Vinicius Gardim <vini.gardim1@gmail.com>
2025-02-06 13:23:52 -0300

    Refinando a busca por filmes

--- curso2-POO/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ curso2-POO/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,11 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=8b8785d3";
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.omdbapi.com/?t=top+gun&apikey=8b8785d3")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
         //client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println).join();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
